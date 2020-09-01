c2ac50cced1fd2dff18ea76b180b74b149b63eef
Jose Jimenez Jr <josejimenezjr0@gmail.com>
2020-09-01 15:10:25 -0500

    add basic server socket structure

--- app.js
+++ app.js
@@ -3,17 +3,22 @@ const app = require('express')()
 const server = require('http').createServer(app)
 const io = require('socket.io')(server)
 
-io.on("connection", (socket) => {
+io.on("connect", (socket) => {
   console.log("New client connected")
-  socket.emit("hello", new Date())
+
+  socket.on("join", async room => {
+    console.log('room: ', room);
+    socket.join(room)
+    io.to(room).emit("newJoin", { room, who: socket.id })
+  })
 
   socket.on("disconnect", () => {
     console.log("Client disconnected");
   })
 
-  socket.on("hello", data => {
+  socket.on("message", async data => {
     console.log(data)
-    socket.emit("hello", new Date())
+    io.to(data.room).emit("sentMessage", { date: new Date(), from: socket.id, message: data.message } )
   })
 })
 
