e13097b30194ceba0a4531fd166c50c69ef683fe
Jose Jimenez Jr <josejimenezjr0@gmail.com>
2020-09-02 09:24:46 -0500

    add basic socket room abilities to start and join

--- app.js
+++ app.js
@@ -3,22 +3,44 @@ const app = require('express')()
 const server = require('http').createServer(app)
 const io = require('socket.io')(server)
 
+let games = []
+
 io.on("connect", (socket) => {
   console.log("New client connected")
 
-  socket.on("join", async room => {
-    console.log('room: ', room);
-    socket.join(room)
-    io.to(room).emit("newJoin", { room, who: socket.id })
+  socket.on("create", async ({ name, players }) => {
+    console.log('create: ', ({ name, players }))
+    console.log('games: ', games)
+    const room = Math.random().toString(36).substring(2, 6)
+    if (games.filter(({ code }) => code === room).length === 0) {
+      const newGame = { code: room, players: [{ name: name, id: socket.id }], size: players }
+      games = [...games, newGame]
+      console.log('games: ', games)
+      socket.join(room)
+      io.to(room).emit("update", newGame )
+    } else {
+      console.log('Room name already exists')
+    }
+    
   })
 
   socket.on("disconnect", () => {
-    console.log("Client disconnected");
+    console.log(`Client: ${socket.id} disconnected`);
   })
 
-  socket.on("message", async data => {
-    console.log(data)
-    io.to(data.room).emit("sentMessage", { date: new Date(), from: socket.id, message: data.message } )
+  socket.on("join", async ({ name, code: room }) => {
+    console.log('join:', ({ name, room }))
+    console.log('games: ', games)
+    let newJoin
+    const findGame = games.filter(({ code }) => code === room)
+    if(findGame) {
+      newJoin = { ...findGame[0], players: [...findGame[0].players, { name: name, id: socket.id }] }
+      games = games.filter(({ code }) => code != room)
+      games = [...games, newJoin]
+    }
+    console.log('games: ', games)
+    socket.join(room)
+    io.to(room).emit("update", newJoin )
   })
 })
 
